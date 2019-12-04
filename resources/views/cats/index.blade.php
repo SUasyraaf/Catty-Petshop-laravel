d2811e651d46aae2932198a89e40a3cd1624a638
SUasyraaf <SUasyraaf>
2019-12-04 22:29:02 +0800

    >Update cats Admin

--- resources/views/cats/index.blade.php
+++ resources/views/cats/index.blade.php
@@ -1,53 +1,77 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cat</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Cats Details
-                    <div class="float-right">
-                        <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Back</a>
-                    </div>
-                </div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+</head>
+<body>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Cats Details
+                        <div class="float-right">
+                            <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Back</a>
                         </div>
-                    @endif
+                    </div>
 
-                    <table class="table">
-                            <thead>
-                              <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>Breed</th>
-                                <th>Age</th>
-                                <th>Action</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                                    @foreach ($cats as $cat)
-                                        <tr>
-                                            <td>{{ $cat->id}}</td>
-                                            <td>{{ $cat->name}}</td>
-                                            <td>{{ $cat->breed}}</td>
-                                            <td>{{ $cat->age}}</td>
-                                            <td>
-                                                <a href="{{ route('cat:show',$cat) }}" class="btn btn-success">Show</a>
-                                                <a href="{{ route('cat:edit',$cat) }}" class="btn btn-primary">Edit</a>
-                                                <a href="{{ route('cat:delete', $cat) }}" class="btn btn-danger"
-                                                    onclick="return confirm('Are you sure?')">Delete</a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                            </tbody>
-                    </table>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <table class="table">
+                                <thead>
+                                  <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Breed</th>
+                                    <th>Age</th>
+                                    <th>Action</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                        @foreach ($cats as $cat)
+                                            <tr>
+                                                <td>{{ $cat->id}}</td>
+                                                <td>{{ $cat->name}}</td>
+                                                <td>{{ $cat->breed}}</td>
+                                                <td>{{ $cat->age}}</td>
+                                                <td>
+                                                    <a href="{{ route('cat:show',$cat) }}" class="btn btn-success">Show</a>
+                                                    <a href="{{ route('cat:edit',$cat) }}" class="btn btn-primary">Edit</a>
+                                                    <a href="{{ route('cat:delete', $cat) }}" class="btn btn-danger"
+                                                        onclick="return confirm('Are you sure?')">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
+
+
