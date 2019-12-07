747ef72a60a16bf9fd61ef65e4f5d1ffda55a815
SUasyraaf <SUasyraaf>
2019-12-07 20:49:29 +0800

    Updated Index interface utk users (Admin)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,57 +1,75 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Room</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Customer details
-                    <div class="float-right">
-                        <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Back</a>
-                    </div>
-                </div>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+</head>
+<body>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Customer details
+                        <div class="float-right">
+                            <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Back</a>
                         </div>
-                    @endif
+                    </div>
 
-                    <table class="table">
-                            <thead>
-                              <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>IC</th>
-                                <th>Email</th>
-                                <th>Address</th>
-                                <th>Phone Number</th>
-                                <th>Action</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                                    @foreach ($users as $user)
-                                        <tr>
-                                            <td>{{ $user->id }}</td>
-                                            <td>{{ $user->name }}</td>
-                                            <td>{{ $user->ic }}</td>
-                                            <td>{{ $user->email }}</td>
-                                            <td>{{ $user->address }}</td>
-                                            <td>{{ $user->phone_num }}</td>
-                                            <td>
-                                                <a href="{{ route('user:show',$user) }}" class="btn btn-success">Show</a>
-                                                <a href="{{ route('user:edit',$user) }}" class="btn btn-primary">Edit</a>
-                                                <a href="{{ route('user:delete', $user)}}" class="btn btn-danger"
-                                                    onclick="return confirm('Are you sure?')">Delete</a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                            </tbody>
-                    </table>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <table class="table">
+                                <thead>
+                                  <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone Number</th>
+                                    <th>Action</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                        @foreach ($users as $user)
+                                            <tr>
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->email }}</td>
+                                                <td>{{ $user->phone_num }}</td>
+                                                <td>
+                                                    <a href="{{ route('user:show',$user) }}" class="btn btn-success">Show</a>
+                                                    <a href="{{ route('user:edit',$user) }}" class="btn btn-primary">Edit</a>
+                                                    <a href="{{ route('user:delete', $user)}}" class="btn btn-danger"
+                                                        onclick="return confirm('Are you sure?')">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
+
+
